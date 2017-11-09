bd1450bc366c95e42729c7ebf53799b4e6038233
Jules Truong <jules.truong.perso@gmail.com>
2017-11-09 12:26:45 +0100

    finally starter works

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import App from './components/App';
 
 ReactDOM.render(<App name="Julio" />, document.getElementById('root'));
--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ export default class App extends React.Component {
     render() {
         return (
             <div style={{textAlign: 'center'}}>
-                <h1>Hello World</h1>
+                <h1>Hello World {this.props.name} </h1>
             </div>
         );
     }
